dcb5d55906914e2eb069ed66b8724ad63df327ed
Mohamed-Maged96 <mohamedmaged291196@gmail.com>
2023-10-15 15:06:19 +0300

    End Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -12,12 +12,20 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseHttpsRedirection();
+// app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200/"));
 
-app.UseAuthorization();
+app.UseCors(p =>
+{
+    p.AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowCredentials()
+    .WithOrigins("https://localhost:4200");
+});
 
 app.MapControllers();
 
